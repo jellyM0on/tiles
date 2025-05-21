725d373f0933b914da977cac14304b3623382176
jellyM0on <ylana.ong@gmail.com>
2025-05-22 03:45:29 +0800

    update styling

--- algo.js
+++ algo.js
@@ -162,12 +162,7 @@ function isSolvable(arr) {
 }
 
 function generateSolvableBoard() {
-  const goalBoard = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 0],
-  ];
-  const maxManhattanDistance = 5;
+  const maxManhattanDistance = 3;
 
   while (true) {
     const values = [0, 1, 2, 3, 4, 5, 6, 7, 8];
